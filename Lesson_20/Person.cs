977c942b3f746b5006bcc7a3bbb69faab3e2fd15
Israel255 <62959042+Israel255@users.noreply.github.com>
2020-08-19 18:03:24 +0300

    Add files via upload

---
+++ Lesson_20/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_20
+{
+    class Person : IComparable<Person>
+    {
+        public int Id { get; private set; }
+        public int Age { get; private set; }
+        public float Height { get; private set; }
+        public string Name { get; private set; }
+
+        public Person(int id, int age, float height, string name)
+        {
+            Id = id;
+            Age = age;
+            Height = height;
+            Name = name;
+        }
+
+        public int CompareTo(Person other)
+        {
+            return this.Id - other.Id;
+        }
+
+        public override string ToString()
+        {
+            return "Person Name: " + Name + " Person Age: " + Age + " Person Height: " + Height + " Person Id: " + Id;
+        }
+    }
+    class PersonCompareById : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Id - y.Id;
+        }
+    }
+    class PersonCompareByAge : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return x.Age - y.Age;
+        }
+    }
+    class PersonCompareByHeight : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            return Convert.ToInt32(x.Height - y.Height);
+        }
+    }
+    class PersonCompareByName : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            int g = x.Name.CompareTo(y.Name);
+            return g;
+        }
+    }
+}
---
+++ Lesson_20/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_20
+{
+    class Program
+    {
+        static void PrintPersonArray(Person[] persons)
+        {
+            foreach (Person p in persons)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person israel = new Person(80948, 25, 1.83f, "Israel");
+            Person lior = new Person(30875928, 23, 1.60f, "Lior");
+            Person adir = new Person(6457, 24, 1.70f, "Adir");
+            Person naor = new Person(24635756, 25, 1.80f, "Naor");
+            Person mor = new Person(436768976, 25, 1.85f, "Mor");
+
+            Person[] persons = { israel, lior, adir, naor, mor };
+
+            PrintPersonArray(persons);
+
+            Console.WriteLine("+++++++++++After sort+++++++++++");
+            Array.Sort(persons);
+            PrintPersonArray(persons);
+
+            Console.WriteLine("+++++++++++By Name++++++++++");
+            Array.Sort(persons,new PersonCompareByName());
+            PrintPersonArray(persons);
+        }
+    }
+}
